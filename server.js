a30c5a65879174b69a76eeaca42d4a4973c28123
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-03-12 03:43:51 +0900

    swagger finished set up

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader(
+     'Acess-Control-allow-Headers',
+     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+   );
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   next()
+ });
 app.use('/', require('./routes'));
 
-// app.use((req, res, next) => {
-//        res.setHeader('Access-Control-Allow-Origin', '*');
-//        res.setHeader(
-//        'Acess-Control-allow-Headers',
-//        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-//        );
-//        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//        next();
-//        })
-//        app.use('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
  if(err) {
--- swagger.js
+++ swagger.js
@@ -1,18 +1,19 @@
 
 const swaggerAutogen = require('swagger-autogen')();
 
-const outputFile = './swagger.json'; // Path where swagger.json will be generated
-const endpointsFiles = ['./routes/*.js']; // Your route files
-
 const doc = {
   info: {
-    title: 'API Documentation', // Title of your API
-    description: 'This is the API documentation generated for our project.', // Description of the API
+    title: 'Contacts API', // Title of your API
+    description: 'Contacts API', // Description of the API
   },
   host: 'localhost:3000', // Your server's host and port
-  schemes: ['http'], // The protocol (http or https)
-  definitions: {} // Definitions for your API models (optional)
+  schemes: ['http','http'], // The protocol (http or https)
+  
 };
+const outputFile = './swagger.json'; // Path where swagger.json will be generated
+const endpointsFiles = ['./routes/index.js']; // Your route files
+
+
 
 // Generate swagger.json file
 swaggerAutogen(outputFile, endpointsFiles, doc);
